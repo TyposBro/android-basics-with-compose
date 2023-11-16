f1ffc03e918826fa72d45bfb620be7804bec281f
typosbro <khasanjonovich@gmail.com>
2023-11-17 00:55:32 +0900

    unit test

--- 03-tip-calculator/app/src/test/java/com/example/tiptime/TipCalculatorTests.kt
+++ 03-tip-calculator/app/src/test/java/com/example/tiptime/TipCalculatorTests.kt
@@ -1,4 +1,16 @@
 package com.example.tiptime
 
+import android.icu.text.NumberFormat
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
 class TipCalculatorTests {
+    @Test
+    fun calculateTip_20PercentNoRoundUp() {
+        val amount = 10.00
+        val tipPercent = 20.00
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        val actualTip = calculateTip(amount, tipPercent, false)
+        assertEquals(actualTip, expectedTip)
+    }
 }
