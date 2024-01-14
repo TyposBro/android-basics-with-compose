91f7989a34942ef212803974b1c87a0c4d5b9b12
typosbro <khasanjonovich@gmail.com>
2024-01-14 20:42:48 +0900

    upgrade

--- 07-unscramble/app/build.gradle.kts
+++ 07-unscramble/app/build.gradle.kts
@@ -23,12 +23,12 @@ plugins {
 
 android {
     namespace = "com.example.unscramble"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.unscramble"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -69,14 +69,14 @@ android {
 
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2023.06.00"))
-    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.activity:activity-compose:1.8.1")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
